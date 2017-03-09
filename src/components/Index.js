1a507fbf59279502f3360b648d4710c26b0c86cc
jeffsenk <jeff.senk@gmail.com>
2017-03-09 15:44:40 -0600

    initial commit

---
+++ src/components/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from './Base';
+
+export default class Index extends React.Component {
+  render(){
+    return (
+      <div className="home">
+        <Base/>
+      </div>
+    );
+  }
+}
