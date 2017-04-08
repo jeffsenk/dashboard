e8cc9604d18cfcdbf475646de4bccde96b2ce753
jeffsenk <jeff.senk@gmail.com>
2017-04-08 11:37:50 -0500

    added chart functionality with recharts

--- src/components/Plot.js
+++ src/components/Plot.js
@@ -1,33 +1,20 @@
 import React from 'react';
+import {LineChart, Line} from 'recharts';
 
 class Plot extends React.Component{
-  componentWillMount(){
-    const script = document.createElement("script");
-    script.src = "https://cdn.plot.ly/plotly-1.8.0.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }
-
-  componentDidMount(){
-    Plotly.newPlot('plot',[{
-      x:[1,2,3,4],
-      y:[1,2,3,4],
-      type: 'scatter'
-    }],{
-      margin: {
-        t:0,r:0,l:30
-      },
-      xaxis:{
-        gridcolor: 'transparent'
-      }
-    },{
-      displayModeBar: false
-    });
-  }
 
   render(){
-    return(
-      <div id="plot"></div>
+
+    const data = [
+      {name: "a",uv: 400},
+      {name: "b",uv: 500},
+      {name: "c",uv: 200}  
+    ]
+ 
+   return(
+      <LineChart width={300} height={300} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#000000" strokeWidth={2}/>
+      </LineChart>
     );
   }
 }
